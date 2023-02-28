d9cd151d0d12ad2ce9f3a506708551d354514e69
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-02-28 14:24:45 -0600

    Finished functionality of strip function's first argument.

--- regexStrip.py
+++ regexStrip.py
@@ -4,10 +4,15 @@
 import re
 
 def spaceStripper(text):
-    frontSpace = re.compile(r'(^\s+)(\w.*\.?)(\s{2,})')
-    spaces = frontSpace.findall(text)
-    print(spaces)
-    print(text)
+    extraSpace = re.compile(r'(^\s+)(\w.*\.+)(\s+$)')
+    spaces = extraSpace.findall(text)
+    front_spaces = spaces[0][0]
+    trimmed_string = spaces[0][1]
+    back_spaces = spaces[0][2]
+    total = len(front_spaces + trimmed_string + back_spaces)
+    print(f"The user input a total of {total} characters.")
+    print(f'A total of {len(front_spaces) + len(back_spaces)} spaces were removed.')
+    print(f'The resulting string ({trimmed_string}) is {len(trimmed_string)} characters in length.')
 
 user_input = input('Please enter spaced out text here: ')
 spaceStripper(user_input)
