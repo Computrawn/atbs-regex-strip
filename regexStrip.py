1542db2aac2d2d8a7ae1def5b183d2e81c929d1f
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-02-28 18:10:25 -0600

    Added pattern match to strip function.

--- regexStrip.py
+++ regexStrip.py
@@ -3,16 +3,22 @@
 
 import re
 
-def spaceStripper(text):
-    extraSpace = re.compile(r'(^\s+)(\w.*\.+)(\s+$)')
-    spaces = extraSpace.findall(text)
-    front_spaces = spaces[0][0]
-    trimmed_string = spaces[0][1]
-    back_spaces = spaces[0][2]
-    total = len(front_spaces + trimmed_string + back_spaces)
-    print(f"The user input a total of {total} characters.")
-    print(f'A total of {len(front_spaces) + len(back_spaces)} spaces were removed.')
-    print(f'The resulting string ({trimmed_string}) is {len(trimmed_string)} characters in length.')
+def strip_Func(userText, userPattern):
+    try:
+        spaceRegex = re.compile(r'(^\s+)(\w.*\.+)(\s+$)')
+        spaces = spaceRegex.findall(userText)
+        front_spaces = spaces[0][0]
+        trimmed_string = spaces[0][1]
+        back_spaces = spaces[0][2]
+        total = len(front_spaces + trimmed_string + back_spaces)
+        print(f"The user input a total of {total} characters.")
+        print(f'A total of {len(front_spaces) + len(back_spaces)} spaces were removed.')
+        print(f'The resulting string ({trimmed_string}) is {len(trimmed_string)} characters in length.')
+    except:
+        patternDetect = re.compile(userPattern)
+        print(patternDetect.sub('', userText))
 
-user_input = input('Please enter spaced out text here: ')
-spaceStripper(user_input)
+user_text = input('Please enter spaced out text here: ')
+user_pattern = input('For pattern strip function, enter pattern. For space strip function, enter None: ')
+
+strip_Func(user_text, user_pattern)
